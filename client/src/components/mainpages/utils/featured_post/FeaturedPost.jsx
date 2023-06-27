1c358976aa1709ce9f6f9e9fcaff6024b007d2bd
Keving <kevingandrades@gmail.com>
2023-06-27 01:58:11 -0400

    changes in featured post

--- client/src/components/mainpages/utils/featured_post/FeaturedPost.jsx
+++ client/src/components/mainpages/utils/featured_post/FeaturedPost.jsx
@@ -4,32 +4,38 @@ import { Link, useNavigate } from 'react-router-dom';
 const FeaturedPost = ({ news, arrow }) => {
 	const navigate = useNavigate();
 	const featuredPost = news.find(news => news.featured);
-	const { _id, title, description, image: { url } } = featuredPost;
 
 	return (
-		<div className='featuredCard'>
+		<>
+			{
+				featuredPost ?
+					<div className='featuredCard'>
 
-			<div className="featuredCard__info">
-				<div className="featuredCard__info--title">{title}</div>
-				<div className="featuredCard__info--description">{description}</div>
-				<div
-					className='featuredCard__info--readMore'
-					onClick={() => navigate(`/post/${_id}`, {
-						state: {
-							post: featuredPost
-						}
-					})}
-				>
-					<span>Leer más</span>
-					{ arrow }
-				</div>
-			</div>
+						<div className="featuredCard__info">
+							<div className="featuredCard__info--title">{featuredPost.title}</div>
+							<div className="featuredCard__info--description">{featuredPost.description}</div>
+							<div
+								className='featuredCard__info--readMore'
+								onClick={() => navigate(`/post/${featuredPost._id}`, {
+									state: {
+										post: featuredPost
+									}
+								})}
+							>
+								<span>Leer más</span>
+								{arrow}
+							</div>
+						</div>
 
-			<div className="featuredCard__img">
-				<img src={url} alt="" onContextMenu={e => e.preventDefault()} draggable={false}/>
-			</div>
+						<div className="featuredCard__img">
+							<img src={featuredPost.image.url} alt="" onContextMenu={e => e.preventDefault()} draggable={false} />
+						</div>
 
-		</div>
+					</div>
+				:
+					null
+			}
+		</>
 	);
 };
 
